4a10d0acbdb9931e9fc7054b77afdaaad0e26c6f
longvha <longvanha@gmail.com>
2021-02-02 00:24:54 +0000

    changes

--- public/app.js
+++ public/app.js
@@ -11,26 +11,29 @@ const getData = async url => {
     return result
 }
 
-getData(api).then( result =>{
+getData(api).then( result => {
     displayList(result) 
     search(result)
-    tags(result)
+    selectTags(result)
     clear(result)
 })
 
 const displayList = result => {
     result.forEach( (obj, index) => {
-        const titleEl = createNew('div', 'title', obj.title)
+        let titleClass = index == 0 ? 'title active' : 'title'
+        const titleEl = createNew('div', titleClass, obj.title)
         const currObj = result[index]
         display(sideListing, titleEl)
         listEvent(titleEl, currObj)
+
+        if (index == 0) displayDishInfo(obj)
     })
 } 
 
 // tags
-const tags = (result) => {
+const selectTags = result => {
     let tags = ""
-    result.forEach( (obj, index) => {        
+    result.forEach( obj => {        
         for(const tag of obj.tags){
             if(tags.indexOf(tag) < 1){
                 tags += `<input type="checkbox" class="tag" name="${tag}"><label for="${tag}">${tag}</label>`
@@ -61,93 +64,162 @@ const tagEvent = result => {
 const handleChecked = (result, tagsChecked) => {
     sideListing.innerHTML = ""
     let dishHasTag = []
-    for(const obj of result){
-        obj.tags.forEach( (tag, index) => {
+
+    result.forEach( (obj, index) => {
+        obj.tags.forEach( tag => {
             if(tagsChecked.includes(tag)) {
                 if(!dishHasTag.includes(obj.title)) dishHasTag.push(obj.title)
             } 
         })
-    }
-    dishHasTag.length > 0 ?  displayTagDishes(result, dishHasTag) : displayList(result)
+    })
+
+   dishHasTag.length > 0 ?  displayTagDishes(result, dishHasTag) : displayList(result)
 }
 
 const displayTagDishes = (result, dishHasTag) => {
+    let counter = 0
+
     result.forEach( (obj, index) => {
         if(dishHasTag.includes(obj.title)){
-            const titleEl = createNew('div', 'title', obj.title)
+            const titleClass = counter == 0 ? 'title active' : 'title'
+            const titleEl = createNew('div', titleClass, obj.title)
             const currObj = result[index]
             display(sideListing, titleEl)
             listEvent(titleEl, currObj)
+            if (counter == 0) displayDishInfo(currObj)
+            counter ++
         }
     })
 }
 
 const listEvent = (el, currObj) => {
-    el.addEventListener('click', () => {
-        let template = ""
-        for(const key in currObj){
-            if (key === 'title') template += handleTitle(currObj, key)
-            else if(key === 'ingredients') template += handleIngredients(currObj, key)
-            else if(key === 'servings') template += handleServings(currObj, key)
-            else if(key === 'author') template += handleAuthor(currObj, key)
-            else template += `<h3>${key.replaceAll('_', ' ')}</h3><p>${currObj[key]}</p>`
-        }
-        const div = createNew('div', 'main', template)
-        main.innerHTML = ""
-        display(main, div)
+    el.addEventListener('click', e => {
+        removeActive()
+        e.target.classList.add('active')
+        displayDishInfo(currObj)
     })
 }
 
+const displayDishInfo = currObj => {
+    let template = ""
+    for(const key in currObj){
+        switch(key) {
+            case 'title':
+                template += title(currObj, key);
+                break;
+            case 'ingredients':
+                template += ingredients(currObj, key);
+                break;
+            case 'directions':
+                template += directions(currObj, key);
+                break;
+            case 'servings':
+                template += servings(currObj, key);
+                break;
+            case 'tags':
+                    template += tags(currObj, key);
+                break;
+            case 'author':
+                template += author(currObj, key);
+                break;
+            case 'source_url':
+                template += source(currObj, key);
+                break;
+            default:
+                template += `<h3>${key.replaceAll('_', ' ')}</h3><p>${currObj[key]}</p>`
+        }
+    }
+    const info = createNew('div', 'main', template)
+    main.innerHTML = ""
+    display(main, info)
+    servingQty()
+}
 
 // Dish information
-const handleTitle = (currObj, key) =>{
+const title = (currObj, key) =>{
    return `<h1>${currObj[key]}</h1>`
 }
 
-const handleIngredients = (currObj, key) => {
-    let temp = `<h3>${key}</h3>`
-    let tempIngr = ""
-    for (const ingr of currObj[key]) tempIngr += `<li>${ingr}</li>`
-    temp += `<ul class="ingredients">${tempIngr}</ul>`
-    return temp
+const ingredients = (currObj, key) => {
+    let ingredients = `<h3>${key}</h3>`
+    let list = ""
+    for (const ingr of currObj[key]) list += `<li>${ingr}</li>`
+    ingredients += `<ul class="ingredients">${list}</ul>`
+    return ingredients
 }
 
-const handleServings = (currObj, key) => {
+const directions = (currObj, key) =>{
+    let directions = `<h3>${key}</h3>`
+    for(const value of currObj[key]) directions += `<p>${value}</p>`
+    return directions
+}
+
+const servings = (currObj, key) => {
     let options = ""
     for(i = 1; i < currObj[key] + 1; i++){
         if(i == currObj[key]) options += `<option value="${i}" selected>${i}</option>`
         else options += `<option value=${i}>${i}</option>`
     }
-    const selectEl = `<h3>${key}</h3><select>${options}</select>`
+    const selectEl = `<h3>${key}</h3><select id="servingQty">${options}</select>`
     return selectEl
 }
 
-const handleAuthor = (currObj, key) =>{
-    for( const k in currObj[key]) return `<h3>${key}</h3>${currObj[key][k]}`
+const tags = (currObj, key) => {
+    let tags = `<h3>${key}</h3>`
+    for(const tag of currObj[key]) tags += `</p>${tag}</p>`
+    return tags
 }
 
+const author = (currObj, key) =>{
+    let author = `<h3>${key}</h3><p>${currObj[key]['name']}</p><p><a href="${currObj[key]['url']}">${currObj[key]['url']}</a></p>`
+    return author
+}
+
+const source = (currObj, key) =>{
+    let source = `<h3>${key.replace('_',' ')}</h3><p><a href="${currObj[key]}">${currObj[key]}</a></p>`
+    return source
+}
+
+const servingQty = () => {
+    const qty = document.querySelector('#servingQty')
+    const ingredients = document.querySelectorAll('.ingredients > li')
+    qty.addEventListener('click', e => {
+      //  console.log('clicked', e.target.value)
+        for(const item of ingredients){
+            const numInString = item.innerHTML.match(/[+-]?\d+(?:\.\d+)?/g)
+            const strSplit = item.innerHTML.split(numInString.length)
+
+            console.log(numInString.length,strSplit[1])
+        }
+    })
+}
 
 // search
-const search = (result) =>{
+const search = result =>{
     searchInput.addEventListener('input', e => {
         sideListing.innerHTML = ""
+        let counter = 0
+        resetChecked()
         result.forEach( (obj, index) => {
             if(JSON.stringify(obj.ingredients).includes(e.target.value)){
-                const titleEl = createNew('div', 'title', obj.title)
+                const titleClass = counter == 0 ? 'title active' : 'title'
+                const titleEl = createNew('div', titleClass, obj.title)
                 const currObj = result[index]
                 display(sideListing, titleEl)
                 listEvent(titleEl, currObj)
+                if(counter == 0) displayDishInfo(currObj)
+                counter ++
             }
         })
     })
 }
 
 // misc
-const display = (section, el) =>{
+const display = (section, el) => {
     section.append(el)
 }
 
-const clear = (result) => {
+const clear = result => {
     clearBtn.addEventListener('click', () => {
         searchInput.value = ""
         sideListing.innerHTML = "" 
@@ -157,8 +229,22 @@ const clear = (result) => {
 
 const createNew = (elemType, elemClass, elemContent ) => {
     const elem = document.createElement(elemType)
-    elem.classList.add(elemClass)
+    elem.className = elemClass
     elem.innerHTML = elemContent
     return elem
 }
 
+const removeActive = () => {
+    const els = document.querySelectorAll('.title')
+    for(const el of els) {
+        if (el.classList.contains('active')){
+            el.classList.remove('active') 
+            return
+        }
+    }
+}
+
+const resetChecked = () => {
+    const tags = document.querySelectorAll('.tag')
+    for(const tag of tags) tag.checked = false
+}
